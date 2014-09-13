ab01f7da0963c0c67d2b463cca8c3b8e80abd44c
sw897 <sunwei.r@gmail.com>
2014-09-13 18:21:34 +0800

    long connect

--- ugeo/__init__.py
+++ ugeo/__init__.py
@@ -1,7 +1,13 @@
 "mix many geo project."
 VERSION = (1, 0, 0)
 
-__author__ = 'sw897'
-__contact__ = "sunwei.r@gmail.com"
+__author__ = 'mapdb'
+__contact__ = "mapdb2014@gmail.com"
 __homepage__ = "https://github.com/sw897/django-ugeo"
 __version__ = ".".join(map(str, VERSION))
+
+from django.core import signals
+from django.db import close_connection
+
+# 取消信号关联，实现数据库长连接
+signals.request_finished.disconnect(close_connection)
